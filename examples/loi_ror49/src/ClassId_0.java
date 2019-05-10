c6e431b6a344b8bbfa7b5e5ae2aaf14a72736b03
Pedro Matheus <pedro.matheus123@gmail.com>
2019-05-10 08:12:05 -0300

    Added dummy code intended to trigger aoui_loi56 drule.

--- examples/loi_ror49/src/ClassId_0.java
+++ examples/loi_ror49/src/ClassId_0.java
@@ -1,34 +1,13 @@
 import java.util.*;
 
 public class ClassId_0 {
-	String[] string;
 	public ClassId_0() {
-		string = new String[2];
-		string[0] = "Hello";
-		string[1] = "World!";
 	}
-	public void loi_ror49() {
-		if (string.length < 0)
+	public void ror_sdl52() {
+		boolean dummy = false;
+		boolean dummy1 = true;
+		if (dummy || dummy1) {
 			System.out.println("Dummy");
-	}
-	public void loi_ror49_1() {
-		if (string.length <= 0)
-			System.out.println("Dummy");
-	}
-	public void loi_ror49_2() {
-		if (string.length == 0)
-			System.out.println("Dummy");
-	}
-	public void loi_ror49_3() {
-		if (string.length > 0)
-			System.out.println("Dummy");
-	}
-	public void loi_ror49_4() {
-		if (string.length >= 0)
-			System.out.println("Dummy");
-	}
-	public void loi_ror49_5() {
-		if (string.length != 0)
-			System.out.println("Dummy");
+		}
 	}
 }
---
+++ examples/ror_sdl52/result/ClassId_0/original/ClassId_0.java
@@ -0,0 +1,61 @@
+// This is a mutant program.
+// Author : ysma
+
+import java.util.*;
+
+
+public class ClassId_0
+{
+
+    java.lang.String[] string;
+
+    public ClassId_0()
+    {
+        string = new java.lang.String[2];
+        string[0] = "Hello";
+        string[1] = "World!";
+    }
+
+    public  void loi_ror49()
+    {
+        if (string.length < 0) {
+            System.out.println( "Dummy" );
+        }
+    }
+
+    public  void loi_ror49_1()
+    {
+        if (string.length <= 0) {
+            System.out.println( "Dummy" );
+        }
+    }
+
+    public  void loi_ror49_2()
+    {
+        if (string.length == 0) {
+            System.out.println( "Dummy" );
+        }
+    }
+
+    public  void loi_ror49_3()
+    {
+        if (string.length > 0) {
+            System.out.println( "Dummy" );
+        }
+    }
+
+    public  void loi_ror49_4()
+    {
+        if (string.length >= 0) {
+            System.out.println( "Dummy" );
+        }
+    }
+
+    public  void loi_ror49_5()
+    {
+        if (string.length != 0) {
+            System.out.println( "Dummy" );
+        }
+    }
+
+}
