d5372ca308c06d749e0b4acbc5a2036b96668d3a
pedro <pmop@ic.ufal.br>
2019-05-10 17:36:47 -0300

    Dummies to trigger drules 66 and 70.

---
+++ examples/ror_ror66/result/ClassId_0/traditional_mutants/void_ror_ror66_strigger()/ROR_1/ClassId_0.java
@@ -0,0 +1,38 @@
+// This is a mutant program.
+// Author : ysma
+
+import java.util.*;
+
+
+public class ClassId_0
+{
+
+    public ClassId_0()
+    {
+    }
+
+    public  void ror_ror66_strigger()
+    {
+        int[] dummy_int = new int[]{ 1, 2, 3, 4, 5, 6 };
+        if (dummy_int.length > 0) {
+            System.out.println( "Dummy" );
+        }
+    }
+
+    public  void ror_ror66_strigger1()
+    {
+        char[] dummy_char = new char[]{ '1', '2', '3', '4', '5', '6' };
+        if (dummy_char.length == 0) {
+            System.out.println( "Dummy" );
+        }
+    }
+
+    public  void ror_ror66_strigger2()
+    {
+        java.lang.String dummy = "Dummy";
+        if (dummy.length() == 0) {
+            System.out.println( "Dummy" );
+        }
+    }
+
+}
